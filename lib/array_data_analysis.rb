2d37c8d4efa7f45059a71329f668257058c104ea
laurenjrichie <laurenjrichie@gmail.com>
2014-11-07 11:37:52 -0800

    refactoring

--- lib/array_data_analysis.rb
+++ lib/array_data_analysis.rb
@@ -2,37 +2,41 @@ class ArrayDataAnalysis
 
   def initialize(data)
     @data = data
+  end
 
-    @low_temps = []
-    @data.each {|day| @low_temps << day[2]}
+  def low_temps
+    low_temps = []
+    @data.each {|day| low_temps << day[2]}
+    low_temps
+  end
 
-    @high_temps = []
-    @data.each {|day| @high_temps << day[1]}
+  def high_temps
+    high_temps = []
+    @data.each {|day| high_temps << day[1]}
+    high_temps
   end
 
   def lowest_temperature
-    @low_temps.min
+    low_temps.min
   end
 
   def highest_temperature
-    @high_temps.max
+    high_temps.max
   end
 
   def day_of_lowest_temperature
-    @low_temps.index(@low_temps.min) + 1
+    low_temps.index(low_temps.min) + 1
   end
 
   def day_of_highest_temperature
-    @high_temps.index(@high_temps.max) + 1
+    high_temps.index(high_temps.max) + 1
   end
 
   def average_temperatures
     ave_temps = []
-
     @data.each do |day|
       ave_temps << (day[1] + day[2])/2.to_f
     end
-
     ave_temps
   end
 
--- lib/class_data_analysis.rb
+++ lib/class_data_analysis.rb
@@ -8,31 +8,32 @@ class ClassDataAnalysis
     @data
   end
 
+  def lows
+    lows = []
+    data.each {|instance| lows << instance.low}
+    lows
+  end
+
+  def highs
+    highs = []
+    data.each {|instance| highs << instance.high}
+    highs
+  end
+
   def lowest_temperature
-    # we have an array of instances of TemperatureData.
-    # iterate through each one and grab value for low
-    # find min.
-    array_of_lows = []
-    data.each {|instance| array_of_lows << instance.low}
-    array_of_lows.min
+    lows.min
   end
 
   def highest_temperature
-    array_of_highs = []
-    data.each {|instance| array_of_highs << instance.high}
-    array_of_highs.max
+    highs.max
   end
 
   def day_of_lowest_temperature
-    array_of_lows = []
-    data.each {|instance| array_of_lows << instance.low}
-    array_of_lows.index(array_of_lows.min) + 1
+    lows.index(lows.min) + 1
   end
 
   def day_of_highest_temperature
-    array_of_highs = []
-    data.each {|instance| array_of_highs << instance.high}
-    array_of_highs.index(array_of_highs.max) + 1
+    highs.index(highs.max) + 1
   end
 
   def average_temperatures
